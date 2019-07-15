bc07a9913cff6cf6f2c1dc2ebeeef914cd1641a5
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-07-14 21:47:53 -0700

    Fixed parallelPCA by using new transposed= option in pca().

--- R/parallelPCA.R
+++ R/parallelPCA.R
@@ -1,19 +1,20 @@
-parallelPCA <- function(x, max.rank=100, ..., niters=50, threshold=0.1, 
+parallelPCA <- function(mat, max.rank=100, ..., niters=50, threshold=0.1, 
   BSPARAM=ExactParam(), BPPARAM=SerialParam()) 
 {
-  original <- pca(y, rank=max.rank, ..., BSPARAM=BSPARAM)
+  mat <- t(mat)
+  original <- pca(mat, rank=max.rank, ..., transposed=TRUE, BSPARAM=BSPARAM)
   original.s2 <- original$variance
 
   # Running across permutations.
   pcg.states <- .setup_pcg_state(niters)
   permuted <- bpmapply(FUN=.parallel_PA,
     seed=pcg.states$seeds[[1]], stream=pcg.states$streams[[1]],
-    MoreArgs=list(y=y, ..., max.rank=max.rank, BSPARAM=BSPARAM), 
+    MoreArgs=list(mat=mat, ..., max.rank=max.rank, BSPARAM=BSPARAM), 
     BPPARAM=BPPARAM, SIMPLIFY=FALSE, USE.NAMES=FALSE)
   permutations <- do.call(cbind, permuted)
 
   # Figuring out where the original drops to "within range" of permuted.
-  prop <- rowMeans(permutations >= original.d2)
+  prop <- rowMeans(permutations >= original.s2)
   above <- prop > threshold
   if (!any(above)) {
     npcs <- length(above) 
@@ -24,12 +25,14 @@ parallelPCA <- function(x, max.rank=100, ..., niters=50, threshold=0.1,
   list(original=original, permuted=permutations, n=npcs)
 }
 
-.parallel_PA <- function(y, max.rank, ..., seed, stream, BSPARAM)
+.parallel_PA <- function(mat, max.rank, ..., seed, stream, BSPARAM)
 # Function for use in bplapply, defined here to automatically 
-# take advantage of the namespace when using snowParam. 
+# take advantage of the namespace when using snowParam. Note 
+# that shuffle_matrix needs samples in columns, hence the t()
+# above and transposed=TRUE in the pca() calls.
 {
-  re.y <- shuffle_matrix(y, seed, stream)
-  out <- pca(re.y, rank=max.rank, ..., BSPARAM=BSPARAM)
+  re.y <- shuffle_matrix(mat, seed, stream)
+  out <- pca(re.y, rank=max.rank, ..., transposed=TRUE, BSPARAM=BSPARAM)
   out$variance
 }
 
