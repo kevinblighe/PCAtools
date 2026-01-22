2b4513ebb787bad5404a66ca894fb38230789377
Jared Andrews <jared.andrews07@gmail.com>
2026-01-22 17:06:55 -0600

    DESCRIPTION fix, namespace fixes

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // shuffle_matrix
 SEXP shuffle_matrix(SEXP incoming, SEXP seed, int stream);
 RcppExport SEXP _PCAtools_shuffle_matrix(SEXP incomingSEXP, SEXP seedSEXP, SEXP streamSEXP) {
